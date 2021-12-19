31e4bb809db1c5579f783eca71e91c6c847957f9
Shakhmardan <190103454@gmail.com>
2021-12-20 05:02:33 +0600

    Executing test only on chrome

--- playwright.config.js
+++ playwright.config.js
@@ -18,15 +18,7 @@ const config = {
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    }
   ],
 };
 
--- src/tests/LogOutTest.spec.js
+++ src/tests/LogOutTest.spec.js
@@ -1,36 +1,44 @@
 const { test, expect } = require("@playwright/test");
-//beforeEach will run before each test
-test.beforeEach(async ({ page }, testInfo) => {
-  testInfo.setTimeout(testInfo.timeout + 90000);
+const { chromium } = require('playwright');
 
-  await page.goto("http://automationpractice.com/index.php");
-  await page.click("a:has-text('Sign in')");
-  await expect(page).toHaveURL(
-    "http://automationpractice.com/index.php?controller=authentication&back=my-account"
-  );
-  await page.fill("input[id=email]", "itspecialbro@gmail.com");
-  await page.fill('input[id="passwd"]', "ShakhMAN12!");
-  await page.click("button[id=SubmitLogin]");
-});
+test.describe("Resident tests", () => {
 
-test("Log out test", async ({ page }) => {
-  const userName = page.locator('//div[@class="header_user_info"]/a/span');
+  let browser,context,page;
 
-  await expect(userName).toHaveText("Shakh Afsdfsfd");
-
-  await expect(page.locator("h1:has-text('My account')")).toBeVisible();
-
-  await expect(
-    page.locator(
-      'p:has-text("Welcome to your account. Here you can manage all of your personal information and orders.")'
-    )
-  ).toBeVisible();
-
-  await page.click('//a[@class="logout"]');
-
-  await expect(page).toHaveURL(
-    "http://automationpractice.com/index.php?controller=authentication&back=my-account"
-  );
-
-  await expect(page.locator('h1:has-text("Authentication")')).toBeVisible();
+    test.beforeAll(async ({},testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 90000);
+    browser = await chromium.launch();
+    context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto("http://automationpractice.com/index.php");
+    await page.click("a:has-text('Sign in')");
+    await expect(page).toHaveURL(
+      "http://automationpractice.com/index.php?controller=authentication&back=my-account"
+    );
+    await page.fill("input[id=email]", "itspecialbro@gmail.com");
+    await page.fill('input[id="passwd"]', "ShakhMAN12!");
+    await page.click("button[id=SubmitLogin]");
+  });
+  
+  test("Log out test", async ({}) => {
+    const userName = page.locator('//div[@class="header_user_info"]/a/span');
+  
+    await expect(userName).toHaveText("Shakh Afsdfsfd");
+  
+    await expect(page.locator("h1:has-text('My account')")).toBeVisible();
+  
+    await expect(
+      page.locator(
+        'p:has-text("Welcome to your account. Here you can manage all of your personal information and orders.")'
+      )
+    ).toBeVisible();
+  
+    await page.click('//a[@class="logout"]');
+  
+    await expect(page).toHaveURL(
+      "http://automationpractice.com/index.php?controller=authentication&back=my-account"
+    );
+  
+    await expect(page.locator('h1:has-text("Authentication")')).toBeVisible();
+  });
 });
